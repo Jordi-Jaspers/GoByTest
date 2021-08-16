99e8c9dbd5a0a9b336bf27b358b99143e3ece62e
jordi-jaspers <34105935+Jordi-Jaspers@users.noreply.github.com>
2021-08-16 22:59:00 +0200

    GO-1: Hello world with different languages and recursion.

--- 1. Hello World/hello_world.go
+++ 1. Hello World/hello_world.go
@@ -1,11 +1,39 @@
 package main
 
+//imports
 import "fmt"
 
-func Hello(name string) string {
-	return "Hello " + name + "! How are you doing?"
+//Constants
+const french = "French"
+const english = "English"
+
+const englishPrefix = "Hello "
+const frenchPrefix = "Bonjour "
+
+const englishAffix = "! How are you doing?"
+const frenchAffix = "! Cava?"
+
+//Global variables
+
+//functions
+
+func Hello(name string, language string) string {
+	if len(name) == 0 {
+		return Hello("world", language)
+	}
+
+	switch language {
+	case french:
+		return frenchPrefix + name + frenchAffix
+
+	case english:
+		return englishPrefix + name + englishAffix
+
+	default:
+		return Hello(name, english)
+	}
 }
 
 func main() {
-	fmt.Println(Hello("Jordi"))
+	fmt.Println(Hello("Jordi", ""))
 }
--- 1. Hello World/hello_world_test.go
+++ 1. Hello World/hello_world_test.go
@@ -10,9 +10,32 @@ import "testing"
 	In order to use the *testing.T type, you need to import "testing"
 */
 func TestHello(t *testing.T) {
-	got := Hello("Jordi")
-	want := "Hello Jordi! How are you doing?"
 
+	//t.run runs function f as a subtest of t
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Jordi", "")
+		want := "Hello Jordi! How are you doing?"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("Say 'Hello world' when an empty string is passed", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello world! How are you doing?"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Elodie", "French")
+		want := "Bonjour Elodie! Cava?"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in English", func(t *testing.T) {
+		got := Hello("Elodie", "English")
+		want := "Hello Elodie! How are you doing?"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got string, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
