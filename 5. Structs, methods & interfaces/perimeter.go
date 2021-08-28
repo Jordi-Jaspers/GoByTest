8d2cf2b8cf9a9ed83afe5d72a846b64dd9ec61e6
jordi-jaspers <34105935+Jordi-Jaspers@users.noreply.github.com>
2021-08-28 15:33:18 +0200

    GO-5: Refactored code by introducin structures.

--- 5. Structs, methods & interfaces/perimeter.go
+++ 5. Structs, methods & interfaces/perimeter.go
@@ -1,9 +1,28 @@
 package perimeter
 
-func Perimeter(height float64, width float64) float64 {
-	return (2 * height) + (2 * width)
+import "math"
+
+type Rectangle struct {
+	Width  float64
+	Height float64
 }
 
-func Area(height float64, width float64) float64 {
-	return height * width
+func (rectangle Rectangle) Perimeter() float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+func (rectangle Rectangle) Area() float64 {
+	return rectangle.Width * rectangle.Height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (circle Circle) Perimeter() float64 {
+	return 2 * math.Pi * circle.Radius
+}
+
+func (circle Circle) Area() float64 {
+	return math.Pi * math.Pow(circle.Radius, 2)
 }
--- 5. Structs, methods & interfaces/perimeter_test.go
+++ 5. Structs, methods & interfaces/perimeter_test.go
@@ -1,20 +1,40 @@
 package perimeter
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
+
+const float64EqualityThreshold = 1e-1
+
+var rectangle = Rectangle{10.0, 10.0}
+var circle = Circle{10.0}
 
 func TestPerimeter(t *testing.T) {
 	t.Run("The perimeter of the rectangle can be calculated.", func(t *testing.T) {
-		got := Perimeter(10.0, 10.0)
+		got := rectangle.Perimeter()
 		want := 40.0
+
+		assertSolution(t, got, want)
+	})
+	t.Run("The perimeter of the circle can be calculated.", func(t *testing.T) {
+		got := circle.Perimeter()
+		want := 62.8
 
 		assertSolution(t, got, want)
 	})
 }
 
 func TestArea(t *testing.T) {
-	t.Run("The are of a rectangle can be calculated.", func(t *testing.T) {
-		got := Area(10.0, 10.0)
+	t.Run("The area of a rectangle can be calculated.", func(t *testing.T) {
+		got := rectangle.Area()
 		want := 100.0
+
+		assertSolution(t, got, want)
+	})
+	t.Run("The area of the circle can be calculated.", func(t *testing.T) {
+		got := circle.Area()
+		want := 314.2
 
 		assertSolution(t, got, want)
 	})
@@ -23,20 +43,22 @@ func TestArea(t *testing.T) {
 func BenchmarkClass(b *testing.B) {
 	b.Run("Benchmarking the perimeter functionality", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
-			Perimeter(10.0, 10.0)
+			rectangle.Perimeter()
+			circle.Perimeter()
 		}
 	})
 	b.Run("Benchmarking the are fucntionality", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
-			Area(10.0, 10.0)
+			rectangle.Area()
+			circle.Area()
 		}
 	})
 }
 
 func assertSolution(t testing.TB, got float64, want float64) {
 	t.Helper()
-	if got != want {
-		t.Errorf("the expected value '%f' does not correspond to the computed value '%f'", got, want)
+	if math.Abs(got-want) >= float64EqualityThreshold {
+		t.Errorf("the expected value '%.1f' does not correspond to the computed value '%.1f'", got, want)
 	}
 
 }
