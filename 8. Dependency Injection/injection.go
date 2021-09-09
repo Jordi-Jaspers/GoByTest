c7a1830408275992fc380861d22b2074480ba3ca
jordi-jaspers <34105935+Jordi-Jaspers@users.noreply.github.com>
2021-09-09 18:02:03 +0200

    GO-8: Basics about dependency injection in Golang

--- 8. Dependency Injection/injection.go
+++ 8. Dependency Injection/injection.go
@@ -1,1 +1,61 @@
-package injection
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+// When you write an HTTP handler, you are given an http.ResponseWriter and
+// the http.Request that was used to make the request.
+// When you implement your server you write your response using the writer.
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	log.Fatal(http.ListenAndServe(":5000", http.HandlerFunc(MyGreeterHandler)))
+}
+
+// =================== NOTES ==========================
+
+/*
+from package "fmt"
+func Fprintf(w io.Writer, format string, a ...interface{}) (n int, err error) {
+    p := newPrinter()
+    p.doPrintf(format, a)
+    n, err = w.Write(p.buf)
+    p.free()
+    return
+}
+
+from package "io"
+type Writer interface {
+    Write(p []byte) (n int, err error)
+}
+
+Buffer type of the package "bytes" implements the Writer interface.
+So it is a child of the Writer type, because it also implementes the Write method.
+
+func (b *Buffer) Write(p []byte) (n int, err error) {
+	b.lastRead = opInvalid
+	m, ok := b.tryGrowByReslice(len(p))
+	if !ok {
+		m = b.grow(len(p))
+	}
+	return copy(b.buf[m:], p), nil
+}
+
+Therefor we implemented the following Greet method,
+but this is not general purpose because we can only pass io.Writer
+or the buffer child but none of the other childeren.
+
+func Greet(writer bytes.Buffer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+*/
--- 8. Dependency Injection/injection_test.go
+++ 8. Dependency Injection/injection_test.go
@@ -1,4 +1,4 @@
-package injection
+package main
 
 import (
 	"bytes"
@@ -6,12 +6,19 @@ import (
 )
 
 func TestGreet(t *testing.T) {
-	buffer := bytes.Buffer{}
-	Greet(&buffer, "Chris")
+	t.Run("Writing a string to the buffer results in the string being saved.", func(t *testing.T) {
+		buffer := bytes.Buffer{}
+		Greet(&buffer, "Chris")
 
-	got := buffer.String()
-	want := "Hello, Chris"
+		got := buffer.String()
+		want := "Hello, Chris"
 
+		AssertSolution(t, got, want)
+	})
+}
+
+func AssertSolution(t testing.TB, got string, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
